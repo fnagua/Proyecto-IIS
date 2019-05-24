905159eb6f209605eac6e8e495a8924a7ba12b09
raulpg1 <raulpeinado10@gmail.com>
2019-05-24 21:46:59 +0200

    Update CPU.java

--- ProyectoIIS(ServerOnly)/src/CPU.java
+++ ProyectoIIS(ServerOnly)/src/CPU.java
@@ -1,4 +1,55 @@
-public class CPU
-{
 
+public class CPU {
+	private String marca;
+	private String modelo;
+	private int frecuenciaBase;//GHz
+	private String socket;
+	private int frecuenciaMem;//MHz
+	private String tecnologiaMem;//DDRX
+	private int maxMem;//Gb
+	private int consumo;//W
+	private int bit;
+	private int precio;
+	public CPU(String marca,String modelo,int frecuenciaBase,String socket,int frecuenciaMem,String tecnologiaMem,int maxMem,int consumo,int bit,int precio){
+		this.marca= marca;
+		this.modelo=modelo;
+		this.frecuenciaBase=frecuenciaBase;
+		this.socket=socket;
+		this.frecuenciaMem=frecuenciaMem;
+		this.tecnologiaMem=tecnologiaMem;
+		this.maxMem=maxMem;
+		this.consumo=consumo;
+		this.bit=bit;
+		this.precio=precio;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public int getFrecuenciaBase() {
+		return frecuenciaBase;
+	}
+	public String getSocket() {
+		return socket;
+	}
+	public int getFrecuenciaMem() {
+		return frecuenciaMem;
+	}
+	public String getTecnologiaMem() {
+		return tecnologiaMem;
+	}
+	public int getMaxMem() {
+		return maxMem;
+	}
+	public int getConsumo() {
+		return consumo;
+	}
+	public int getBit() {
+		return bit;
+	}
+	public int getPrecio() {
+		return precio;
+	}
 }
