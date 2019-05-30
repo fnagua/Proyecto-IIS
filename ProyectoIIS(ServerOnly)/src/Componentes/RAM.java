779f6a921371ba06ad28efc9e86fb1b8c5729b23
daviddr1999 <daviddr@uma.es>
2019-05-30 18:18:46 +0200

    improved CPU

--- ProyectoIIS(ServerOnly)/src/Componentes/RAM.java
+++ ProyectoIIS(ServerOnly)/src/Componentes/RAM.java
@@ -2,8 +2,34 @@ package Componentes;
 
 public class RAM
 {
+
 	private String nombre;//marca+modelo
 	private float precio;
 	private int capacidad;
 	private int frecuencia;
+
+	public RAM(String nombre, float precio, int capacidad, int frecuencia) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.capacidad = capacidad;
+		this.frecuencia = frecuencia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public int getFrecuencia() {
+		return frecuencia;
+	}
+
+
 }
