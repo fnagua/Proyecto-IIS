8d106ae667b02fdd826c5d8e77b034b61c813ffe
daviddr1999 <daviddr@uma.es>
2019-05-30 19:15:38 +0200

    Carcasa añadida

--- ProyectoIIS(ServerOnly)/src/Componentes/Carcasa.java
+++ ProyectoIIS(ServerOnly)/src/Componentes/Carcasa.java
@@ -7,4 +7,52 @@ public class Carcasa {
 	private float tamFuente;
 	private int ilum;//0 = no incorpora; 1 = incorpora
 
+	public Carcasa(String nombre, float precio, float tamPlaca, float tamFuente, int ilum) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.tamPlaca = tamPlaca;
+		this.tamFuente = tamFuente;
+		this.ilum = ilum;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public float getTamPlaca() {
+		return tamPlaca;
+	}
+
+	public float getTamFuente() {
+		return tamFuente;
+	}
+
+	public int getIlum() {
+		return ilum;
+	}
+
+	public boolean getCompatibilidad(PC pc) {
+		boolean res = false;
+
+
+		if ( pc.getPlacaBase().getTamaño() <= this.tamPlaca && pc.getFuenteAlimentacion().getTamaño() <= this.tamFuente && cumplePresupuesto(pc)) {
+			res = true;
+		}
+
+		return res;
+	}
+
+	private boolean cumplePresupuesto(PC pc) {
+		boolean res = false;
+
+		if (pc.getPrecioAcumulado()+this.precio <= pc.getPrecioMax()) {
+			res = true;
+		}
+
+		return res;
+	}
 }
