b8d8a3d06f0ca4034f63a47d74a3d0d0f0ff5ce4
H1er <mejorjesus.99@gmail.com>
2019-05-30 19:03:08 +0200

    Placa Base donete

--- ProyectoIIS(ServerOnly)/src/Componentes/PlacaBase.java
+++ ProyectoIIS(ServerOnly)/src/Componentes/PlacaBase.java
@@ -1,10 +1,86 @@
 package Componentes;
 
-public class PlacaBase
+public class PlacaBase implements Componente
 {
 	private String nombre;//marca+modelo
 	private float precio;
 	private String socket;
 	private int frecuenMem;//MHz
-	private int tamaño;
+	private int tamanio;
+
+	public PlacaBase(String nombre, String socket, int freq, int tamanio, float precio)
+	{
+		this.nombre = nombre;
+		this.socket = socket;
+		this.frecuenMem = freq;
+		this.tamanio = tamanio;
+		this.precio = precio;
+	}
+
+	public String getNombre()
+	{
+		return nombre;
+	}
+
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+
+	public float getPrecio()
+	{
+		return precio;
+	}
+
+	public void setPrecio(float precio)
+	{
+		this.precio = precio;
+	}
+
+	public String getSocket()
+	{
+		return socket;
+	}
+
+	public void setSocket(String socket)
+	{
+		this.socket = socket;
+	}
+
+	public int getFrecuenMem()
+	{
+		return frecuenMem;
+	}
+
+	public void setFrecuenMem(int frecuenMem)
+	{
+		this.frecuenMem = frecuenMem;
+	}
+
+	public int getTamaño()
+	{
+		return tamanio;
+	}
+
+	public void setTamaño(int tamaño)
+	{
+		this.tamanio = tamaño;
+	}
+
+	@Override
+	public boolean getCompatibilidad(PC pc)
+	{
+		boolean ok = false;
+		if(pc.getRAM().getFrecuencia()<=this.frecuenMem && cumplePresupuesto(pc))
+		{
+
+		}
+		return false;
+	}
+
+	public boolean cumplePresupuesto(PC pc)
+	{
+		return (pc.getPrecioAcumulado()+this.precio)<pc.getPrecioMax();
+
+	}
 }
