c51d3b8f9f69d8ede2fdc566ce9259065486fc5e
daviddr1999 <daviddr@uma.es>
2019-05-30 19:26:10 +0200

    Carcasa añadida

--- ProyectoIIS(ServerOnly)/src/Componentes/Tarjeta_Red.java
+++ ProyectoIIS(ServerOnly)/src/Componentes/Tarjeta_Red.java
@@ -4,4 +4,38 @@ public class Tarjeta_Red
 {
     private String nombre;//marca+modelo
     private float precio;
+
+    public Tarjeta_Red(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public boolean getCompatibilidad(PC pc) {
+        boolean res = false;
+
+
+        if ( cumplePresupuesto(pc) ) {
+            res = true;
+        }
+
+        return res;
+    }
+
+    private boolean cumplePresupuesto(PC pc) {
+        boolean res = false;
+
+        if (pc.getPrecioAcumulado()+this.precio <= pc.getPrecioMax()) {
+            res = true;
+        }
+
+        return res;
+    }
 }
